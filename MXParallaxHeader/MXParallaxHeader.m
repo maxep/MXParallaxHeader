0d49ddd4160aa8a2ac4e4daefaa5eff3899641b0
Maxime Epain <maxime@mapstr.com>
2016-03-07 11:05:30 +0100

    Fix constraints conflict

--- MXParallaxHeader/MXParallaxHeader.m
+++ MXParallaxHeader/MXParallaxHeader.m
@@ -180,25 +180,11 @@ static void * const kMXParallaxHeaderKVOContext = (void*)&kMXParallaxHeaderKVOCo
 }
 
 - (void) setTopFillModeConstraints {
-    NSDictionary *binding  = @{@"v" : self.view};
+    NSDictionary *binding   = @{@"v" : self.view};
+    NSDictionary *metrics   = @{@"highPriority" : @(UILayoutPriorityDefaultHigh),
+                                @"height"       : @(self.height)};
     [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[v]|" options:0 metrics:nil views:binding]];
-    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[v]|" options:0 metrics:nil views:binding]];
-    
-    [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.view
-                                                                 attribute:NSLayoutAttributeTop
-                                                                 relatedBy:NSLayoutRelationEqual
-                                                                    toItem:self.contentView
-                                                                 attribute:NSLayoutAttributeTop
-                                                                multiplier:1
-                                                                  constant:0]];
-    
-    [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.view
-                                                                 attribute:NSLayoutAttributeHeight
-                                                                 relatedBy:NSLayoutRelationGreaterThanOrEqual
-                                                                    toItem:nil
-                                                                 attribute:NSLayoutAttributeNotAnAttribute
-                                                                multiplier:1
-                                                                  constant:self.height]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[v(>=height)]-0.0@highPriority-|" options:0 metrics:metrics views:binding]];
 }
 
 - (void) setTopModeConstraints {
