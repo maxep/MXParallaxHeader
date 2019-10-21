f0a3d4c93f50dbf569953fa4bc51be8e3c348e14
Keith Lamprecht <1894492+Nixon506E@users.noreply.github.com>
2019-10-21 13:19:09 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
     targets: [
         .target(
             name: "MXParallaxHeader",
+            dependencies:["objc"],
             path: "MXParallaxHeader"
         )
     ],
