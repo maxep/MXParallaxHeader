fc18f4d1459b24c943fd85c931c1b2d35eac9f80
kenji21 <r.bergoin@openium.fr>
2020-07-30 13:35:49 +0200

    Set public headers to make all .h files public (#122)

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
         .target(
             name: "MXParallaxHeader",
             dependencies: [],
-            path: "Sources")
+            path: "Sources",
+            publicHeadersPath: ".")
     ]
 )
